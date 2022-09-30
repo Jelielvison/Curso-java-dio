02b58b4eebd3488ce4b0cfa93b875512ac7ed1f4
Jelielvison <jelielfloresmoura@gmail.com>
2022-09-30 20:30:17 -0300

    Novo método

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -8,10 +8,13 @@ public static void main (String [] args) {
     String nomeCompleto = nomeCompleto(primeiroNome, segundoNome, terceiroNome);
     System.out.println(nomeCompleto);
 
+
+    System.out.println("Sem método: " + primeiroNome.concat(" ").concat(segundoNome).concat(" ").concat(terceiroNome));
+
 }
 
 public static String nomeCompleto (String primeiroNome, String segundoNome, String terceiroNome) {
-    return "Meu nome é: " + primeiroNome.concat(" ").concat(segundoNome).concat(" ").concat(terceiroNome);
+    return "Com método: " + primeiroNome.concat(" ").concat(segundoNome).concat(" ").concat(terceiroNome);
 }
 
 }
