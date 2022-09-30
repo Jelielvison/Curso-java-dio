ddc62515627488545d043a12b1c025c83d0975c8
Jelielvison <jelielfloresmoura@gmail.com>
2022-09-30 20:29:26 -0300

    Apresentar nome

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,7 +1,17 @@
 public class MinhaClasse {
     
 public static void main (String [] args) {
-    System.out.print("Primeira classe criada");
+    String primeiroNome = "Jeliel";
+    String segundoNome = "Flores";
+    String terceiroNome = "Moura";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome, terceiroNome);
+    System.out.println(nomeCompleto);
+
+}
+
+public static String nomeCompleto (String primeiroNome, String segundoNome, String terceiroNome) {
+    return "Meu nome é: " + primeiroNome.concat(" ").concat(segundoNome).concat(" ").concat(terceiroNome);
 }
 
 }
